40f4c41615a13868d4a372ebe891450a5bdb2ffb
Namarich <142319833+Namarich@users.noreply.github.com>
2025-03-28 14:25:43 +0700

    bouncyBoi

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
 
-    public float speed;
+    public float MaxSpeed;
+    private float speed;
 
     private bool wasPressed;
     private bool wasCollision;
@@ -16,10 +17,16 @@ public class Ball : MonoBehaviour
 
     private float ZRot;
 
+    private Transform startTransform;
+
+    private Vector2 direction;
+
+    private Vector3 mousePos;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Reset();
     }
 
     // Update is called once per frame
@@ -28,34 +35,48 @@ public class Ball : MonoBehaviour
 
         if (!wasPressed)
         {
-            Vector3 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+            mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
             Vector3 perpendicular = transform.position - mousePos;
             transform.rotation = Quaternion.LookRotation(Vector3.forward, perpendicular);
         }
-        
-
-
 
         if (Input.GetMouseButtonDown(0) && !wasPressed)
         {
             //rb.AddForce(new Vector2(10,10));
             wasPressed = true;
-            
+            startTransform = gameObject.transform;
+            direction = mousePos - transform.position;
         }
 
         if (wasPressed)
         {
-            transform.position += transform.up * -1 * Time.deltaTime * speed;
+            //transform.position += transform.up * -1 * Time.deltaTime * speed;
+            transform.position += (Vector3)direction.normalized * speed * Time.deltaTime;
             //transform.right
+        }
+
+        if (speed <= MaxSpeed * 0.1)
+        {
+            Reset();
         }
             
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        //Debug.Log($"{transform.eulerAngles.z};{transform.eulerAngles.z-360}");
-        //transform.rotation = Quaternion.Euler(0,0,transform.rotation.z-360);
-        transform.eulerAngles = new Vector3(0, 0, 180-transform.eulerAngles.z);
-        speed = speed * speedReduction;
+
+        Vector2 surfaceNormal = collision.contacts[0].normal;
+        direction = Vector2.Reflect(direction, surfaceNormal);
+        speed *= speedReduction;
+        Vector3 perpendicular = transform.position - (Vector3)direction;
+        transform.rotation = Quaternion.LookRotation(Vector3.forward, perpendicular);
+    }
+
+    public void Reset()
+    {
+        speed = MaxSpeed;
+        transform.position = new Vector3(0, 0, 0);
+        transform.eulerAngles = new Vector3(0, 0, 0);
+        wasPressed = false;
     }
 }
